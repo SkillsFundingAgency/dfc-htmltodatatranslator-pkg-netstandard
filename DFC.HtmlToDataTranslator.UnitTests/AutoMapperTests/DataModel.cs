5c2cea4eb7d33e1b0bbe2f4fadd5cb952f843e53
ilyasdhin <ilyasdhin@gmail.com>
2019-11-08 14:44:35 +0000

    Added nuget package and automapper tests (#1)
    * Added nuget package and automapper tests
    
    * Incremented package version

---
+++ DFC.HtmlToDataTranslator.UnitTests/AutoMapperTests/DataModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DFC.HtmlToDataTranslator.UnitTests.AutoMapperTests
+{
+    public class DataModel
+    {
+        public Guid Id { get; set; }
+
+        public string Markup { get; set; }
+    }
+}
---
+++ DFC.HtmlToDataTranslator.UnitTests/AutoMapperTests/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DFC.HtmlToDataTranslator.UnitTests.AutoMapperTests
+{
+    public class ViewModel
+    {
+        public Guid Id { get; set; }
+
+        public List<string> Markup { get; set; }
+    }
+}
