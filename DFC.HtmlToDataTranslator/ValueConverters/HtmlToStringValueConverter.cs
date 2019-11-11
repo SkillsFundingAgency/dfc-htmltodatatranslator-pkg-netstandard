d9f13bb94703e2716c22e41b01fe34c851261205
ilyasdhin <ilyasdhin@gmail.com>
2019-11-11 09:48:32 +0000

    Updated nuget and added htmltostringvalueconverter (#2)
    * Updated nuget and added htmltostringvalueconverter
    
    * Updated package version

---
+++ DFC.HtmlToDataTranslator/ValueConverters/HtmlToStringValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DFC.HtmlToDataTranslator.Contracts;
+using DFC.HtmlToDataTranslator.Services;
+using System.Collections.Generic;
+
+namespace DFC.HtmlToDataTranslator.ValueConverters
+{
+    public class HtmlToStringValueConverter : IValueConverter<string, List<string>>
+    {
+        private readonly IHtmlToDataTranslator htmlToDataTranslator;
+
+        public HtmlToStringValueConverter()
+        {
+            this.htmlToDataTranslator = new HtmlAgilityPackDataTranslator();
+        }
+
+        public List<string> Convert(string sourceMember, ResolutionContext context)
+        {
+            var result = htmlToDataTranslator.Translate(sourceMember);
+            return result;
+        }
+    }
+}
