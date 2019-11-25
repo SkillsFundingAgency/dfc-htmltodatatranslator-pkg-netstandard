48d52be0a9822d51f4e6b2f36251205df3440eae
ilyasdhin <ilyasdhin@gmail.com>
2019-11-25 14:17:17 +0000

    Dev ilyas 11099 (#11)
    * Added pre processing rules
    
    * Updated package version

---
+++ DFC.HtmlToDataTranslator/Services/RulesProcessor.cs
@@ -0,0 +1,28 @@
+﻿using DFC.HtmlToDataTranslator.Contracts;
+using DFC.HtmlToDataTranslator.Rules;
+using System.Collections.Generic;
+
+namespace DFC.HtmlToDataTranslator.Services
+{
+    public class RulesProcessor
+    {
+        public string Process(string sourceValue)
+        {
+            var result = sourceValue;
+            var rules = GetRules();
+            foreach (var rule in rules)
+            {
+                result = rule.Process(result);
+            }
+
+            return result;
+        }
+
+        private IEnumerable<IPreProcessorRule> GetRules()
+        {
+            var rules = new List<IPreProcessorRule>();
+            rules.Add(new ReplaceBrTagsWithPTagsRule());
+            return rules;
+        }
+    }
+}
